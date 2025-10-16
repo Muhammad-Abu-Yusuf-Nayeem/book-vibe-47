c8c2f41b796cfa10bee4fbbb9ec1e8b162ffa216
assignment-01 <ykhan9463@gmail.com>
2025-10-16 10:50:04 +0600

    47-3 load fake data to display books -47-4 Book Card And Responsive Card Layout

---
+++ src/components/Books.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import Book from "./Book";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("../../public/data/booksData.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  return (
+    <>
+      <h2 className="text-4xl font-bold text-center">Books</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6 mx-12 ">
+        {books.map((book) => (
+          <Book key={book.bookId} book={book}></Book>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Books;
+
+/**
+ * 1. state to store books data
+ * 2. useEffect to load data
+ * 3. fetch to load data
+ * set the data to the state
+ */
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
+import Books from './Books';
 
 const Home = () => {
     return (
         <div>
-            <h2>this is home page</h2>
+            <Banner></Banner>
+            <Books></Books>
         </div>
     );
 };
