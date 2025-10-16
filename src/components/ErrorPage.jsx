ce5e8f8501538c8af7344987be2d77c26c9beac4
assignment-01 <ykhan9463@gmail.com>
2025-10-16 09:48:06 +0600

    47-1 React Router Tailwind Daisy Ui Setup For Boi Poka -47-2 nested route and navbar setup

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="text-center mt-20">
+      <h2 className=" text-5xl font-bold">Page not found!!</h2>
+      <h3 className=" text-3xl mt-5">
+        Status: 404 </h3>
+    </div>
+  );
+};
+
+export default ErrorPage;
